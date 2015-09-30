4f6d120ffd006bbe3a3edb3c08a83b0e5d5c8430
Jonathan Porter <myctrring@gmail.com>
2015-09-29 18:48:45 -0600

    frogs almost done

--- app.js
+++ app.js
@@ -4,44 +4,74 @@ app.controller('MainController', MainController);
 
 function MainController() {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
+    
+    vm.frogs = [
+        { name: 'pluto', position: 0 },
+        { name: 'jupiter', position: 0 },
+        { name: 'mars', position: 0 }
+    ];
 
-    vm.joe = new Guy("Joe", 100)
-    vm.bob = new Guy("Bob", 150)
-    vm.bank = 200;
+    function random () {
+        return Math.random() * 5 + 1;
+    }
 
-    function Guy(name, startingCash) {
-        this.name = name;
-        this.cash = startingCash;
-        this.giveCash = function(amount) {
-            if (amount <= this.cash && amount > 0) {
-                this.cash = this.cash - amount;
-                return amount;
-            } else {
-                console.log("I don't have enough cash to give you " + amount + ". " + this.name + " says...");
-                return 0;
-            }
-        };
-        this.receiveCash = function(amount) {
-            if (amount > 0) {
-                this.cash = this.cash + amount;
-                return amount;
-            } else {
-                console.log(amount + " isn't an amount I'll take " + this.name + " says...");
-                return 0;
-            }
+
+    vm.race = function () {
+        vm.frogs.forEach(function (frog) {
+            frog.position += random();
+            if (frog.position >= 95) {
+                alert(frog.name + ' wins!')
+                
+                
         }
-    }
 
-    vm.giveMoneyToJoe = function() {
-        if (vm.bank >= 10) {
-            vm.bank -= vm.joe.receiveCash(10)
-        } else {
-            alert("The bank is out of money.")
-        }
+        })
     }
-
-    vm.receiveMoneyFromBob = function() {
-        vm.bank += vm.bob.giveCash(5)
-    }
-
 }
+
+
+
+
+
+
+    /*  vm.joe = new Guy("Joe", 100)
+        vm.bob = new Guy("Bob", 150)
+        vm.bank = 200;
+    
+        function Guy(name, startingCash) {
+            this.name = name;
+            this.cash = startingCash;
+            this.giveCash = function(amount) {
+                if (amount <= this.cash && amount > 0) {
+                    this.cash = this.cash - amount;
+                    return amount;
+                } else {
+                    console.log("I don't have enough cash to give you " + amount + ". " + this.name + " says...");
+                    return 0;
+                }
+            };
+            this.receiveCash = function(amount) {
+                if (amount > 0) {
+                    this.cash = this.cash + amount;
+                    return amount;
+                } else {
+                    console.log(amount + " isn't an amount I'll take " + this.name + " says...");
+                    return 0;
+                }
+            }
+        }
+    
+        vm.giveMoneyToJoe = function() {
+            if (vm.bank >= 10) {
+                vm.bank -= vm.joe.receiveCash(10)
+            } else {
+                alert("The bank is out of money.")
+            }
+        }
+    
+        vm.receiveMoneyFromBob = function() {
+            vm.bank += vm.bob.giveCash(5)
+        }
+        */
+
+    
